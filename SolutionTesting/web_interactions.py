4a488cf2a0e20710b7cc0e19cac5fac910d054aa
Zac Todd <zactodd0@gmail.com>
2019-12-29 14:44:35 +1300

    driver installiation

--- SolutionTesting/web_interactions.py
+++ SolutionTesting/web_interactions.py
@@ -1,8 +1,27 @@
+import sys
+import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-DRIVER = webdriver.Chrome(ChromeDriverManager().install())
+def get_chrome_driver():
+    """
+    Installs the chrome driver if already installed gets the drivers path.
+    :return: The driver.
+    """
+    sys.stdout = open(os.devnull, 'w')
+    try:
+        path = ChromeDriverManager().install()
+    except PermissionError as e:
+        if e.errno != 13:
+            raise e
+        path = e.filename
+    finally:
+        sys.stdout = sys.__stdout__
+    return webdriver.Chrome(path)
+
+
+DRIVER = get_chrome_driver()
 URL = "https://play.elevatorsaga.com/"
 LEVEL_FORMAT = "#challenge={}"
 
