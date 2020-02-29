4a3c722da471a1485c2848255acd218ea1528db1
Zac Todd <zactodd0@gmail.com>
2020-02-29 21:08:21 +1300

    Update web_interactions.py

--- SolutionTesting/web_interactions.py
+++ SolutionTesting/web_interactions.py
@@ -1,26 +1,5 @@
-import sys
-import os
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from utils import get_chrome_driver
 from selenium.common.exceptions import NoSuchElementException
-
-
-def get_chrome_driver():
-    """
-    Installs the chrome driver if already installed gets the drivers path.
-    :return: The driver.
-    """
-    sys.stdout = open(os.devnull, 'w')
-    try:
-        path = ChromeDriverManager().install()
-    except PermissionError as e:
-        if e.errno != 13:
-            raise e
-        path = e.filename
-    finally:
-        sys.stdout = sys.__stdout__
-    return webdriver.Chrome(path)
-
 
 DRIVER = get_chrome_driver()
 URL = "https://play.elevatorsaga.com/"
